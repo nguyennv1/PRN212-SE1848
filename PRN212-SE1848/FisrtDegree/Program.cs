32b61c917144d273edac1fb6580e937c671acff1
nvnguyen1 <tinh4493@gmail.com>
2025-05-26 14:43:42 +0700

    Initial commit

---
+++ PRN212-SE1848/FisrtDegree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+void first_degree_solution(double a, double b)
+{
+    if (a == 0 && b == 0)
+    {
+        Console.WriteLine("Vô số nghiệm");
+    }
+    else if (a == 0 && b != 0)
+    {
+        Console.WriteLine("Vô nghiệm");
+    }
+    else
+    {   
+        Console.WriteLine("X={0}", -b/a);
+    }
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Phương trình bậc một: ax + b = 0");
+Console.WriteLine("Nhập hệ số a:");
+double a =double.Parse(Console.ReadLine());
+Console.WriteLine("Nhập hệ số b:");
+double b =double.Parse(Console.ReadLine());
+
+Console.WriteLine("{0}x+{1}", a, b);
+first_degree_solution(a, b);
+Console.ReadLine();
---
+++ PRN212-SE1848/ParamsSample/Program.cs
@@ -0,0 +1,11 @@
+﻿int sum(params int[] arr)
+{
+    int s = 0;
+    foreach (int x in arr)
+        s = s + x;
+    return s;
+}
+int s0 = sum();
+int s1 = sum(100);
+int s2 = sum(100, 200);
+int s3 = sum(3, 4, 20, 200, 42, 34);
